750ad240dae2f0b20f0d44fb782b4556b47a0a11
Oscar Dragén <oscar.dragen@gmail.com>
2025-05-17 13:24:58 +0200

    feat: Add eq and hash functions

--- chord.py
+++ chord.py
@@ -18,3 +18,17 @@ class Chord:
         Checks if the named chord is a part of this chord.
         """
         return name in self.alternative_names
+
+    def __eq__(self, other):
+        """
+        Checks equality between two Chord objects based on their alternative names.
+        """
+        if not isinstance(other, Chord):
+            return False
+        return set(self.alternative_names) == set(other.alternative_names)
+
+    def __hash__(self):
+        """
+        Returns a hash based on the chord's alternative names.
+        """
+        return hash(tuple(sorted(self.alternative_names)))
